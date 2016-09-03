12838d53333f318906b42dafd2cc93e2173ca68b
xtengf <xtengf@gmail.com>
2016-09-03 20:36:14 +0800

    update

--- Java/think java/Log2final.java
+++ Java/think java/Log2final.java
@@ -1,10 +1,11 @@
-public class LogE {
+public class Log2final {
     
     public static void table(int n) {
+        final double LOG2 = Math.log(2);
         int i = 1;
         while (i < 10) {
             double x = (double) i;
-            System.out.println(x + "    " + Math.log(x));
+            System.out.println(x + "    " + Math.log(x) / LOG2);
             i = i + 1;
         }
     }
